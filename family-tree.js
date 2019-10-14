df7ca1c9d62a28bb53909815eca74e60b8408ed5
Deanna Wong <dwong221@gmail.com>
2019-10-13 23:32:20 -0400

    Almost complete solution

--- family-tree.js
+++ family-tree.js
@@ -1,5 +1,46 @@
 class FamilyTree {
-  constructor () {}
+  constructor (value) {
+    if(!value)throw 'Must have parent value!';
+    if(typeof value!== 'string')throw 'Value must be string!'
+
+    this.value=value;
+    this.children=[];
+  }
+  insert(child){
+    this.children.push(new FamilyTree(child));
+  }
+  familySize(){
+    return this.children.length+1;
+  }
+  findMember(familyMember){
+
+    return this.children.find(person=>{
+
+      return person.value===familyMember;
+
+    })
+
+  }
+
+  log(){
+    let log=[];
+
+    if(this.children.length===0){
+      log.push(`-- ${this.value}`)
+    }else{
+      
+      let family = this.children.map(child=>{
+        return `--${child.log()}`
+      })
+
+      family.unshift(`-- ${this.value}`);
+      
+      log.push(family.join(''))
+
+    }
+    return log
+  }
+
 }
 
 module.exports = FamilyTree;
